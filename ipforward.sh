e82127027753cd9d1eaf87d4fc46a5d071ec9ff0
hackingyseguridad <antonio_taboada@hotmail.es>
2018-12-16 17:45:15 +0100

    Update ipforward.sh

--- ipforward.sh
+++ ipforward.sh
@@ -3,12 +3,10 @@
 # Activa IP forwarding en Linux
 # Convierte Linux en un en un enrutador/ puerta de enlace 
 # www.hackingyseguridad.com
-echo .
+echo ...
 echo ipv4 >> /etc/modules
-echo .
 echo ipv6 >> /etc/modules
 sysctl -w net.ipv4.ip_forward=1
-echo .
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
 sysctl -p
