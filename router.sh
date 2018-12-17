ccd17e9cd689c14d328bf66500824668462d478f
hackingyseguridad <antonio_taboada@hotmail.es>
2018-12-17 11:31:23 +0100

    Create router.sh

---
+++ router.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Activa IP forwarding en Linux / NAT
+# Convierte Linux en un en un enrutador/ puerta de enlace
+# www.hackingyseguridad.com
+echo ...
+echo ipv4 >> /etc/modules
+echo ipv6 >> /etc/modules
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
+sysctl -p
+echo
+echo "===================================================="
+echo "   POR FAVOR REINICIALIZA PARA QUE TENGA EFECTO !   "
+echo "===================================================="
+iptables -L FORWARD -nv
+iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
