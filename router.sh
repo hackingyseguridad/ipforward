08867e378c4a219f3051b3d1ddd68b973c1cab6c
hackingyseguridad <antonio_taboada@hotmail.es>
2018-12-17 23:09:15 +0100

    Update router.sh

--- router.sh
+++ router.sh
@@ -3,7 +3,23 @@
 # Activa IP forwarding en Linux / NAT
 # Convierte Linux en un en un enrutador/ puerta de enlace
 # www.hackingyseguridad.com
+Blanco=$(echo 'printf' '\033[0m')
+BlancoAlt=$(echo 'printf' '\033[97m')
+$Blanco
+cat << "INFO"
+  _____             _            
+ |  __ \           | |           
+ | |__) |___  _   _| |_ ___ _ __ 
+ |  _  // _ \| | | | __/ _ \ '__|
+ | | \ \ (_) | |_| | ||  __/ |   
+ |_|  \_\___/ \__,_|\__\___|_|  V 1.0 
+                                
+INFO
+$BlancoAlt
+echo "http://www.hackingyseguridad.com"
+echo
 echo ...
+$Blanco
 echo ipv4 >> /etc/modules
 echo ipv6 >> /etc/modules
 sysctl -w net.ipv4.ip_forward=1
