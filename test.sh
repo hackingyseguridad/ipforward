8332aacb9eeec62280452e9621043ff88dc19866
hackingyseguridad <antonio_taboada@hotmail.es>
2018-12-20 15:13:44 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #Test IP Forwading esta activado en host remoto.
+#
 #!/bin/bash
 for n in `cat ip.txt`
 do
 
 route add default gw $n
-ping 8.8.8.8
-route del default gw $n
+if ping 8.8.8.8 -c 1 -W 1 > /dev/null
+then echo $n "IP forwarding up"
+else echo $n "down"
 
 done
