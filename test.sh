ff11b0e95c8b2f2b6d944b1c6e8ac2cb897f70f3
hackingyseguridad <antonio_taboada@hotmail.es>
2018-12-20 15:24:24 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,11 +2,8 @@
 #
 #!/bin/bash
 for n in `cat ip.txt`
-do
-
-route add default gw $n
+do route add default gw $n
 if ping 8.8.8.8 -c 1 -W 1 > /dev/null
 then echo $n "IP forwarding up"
-else echo $n "down"
-
+fi
 done
